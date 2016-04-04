db58751ab1a751f74a9637f193de2b7f1a76d6b8
bsk <bensamuelskalow@gmail.com>
2016-04-04 14:00:08 -0400

    stroke weight

--- DrawingApp/sketch.js
+++ DrawingApp/sketch.js
@@ -5,7 +5,7 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    strokeWeight(abs(mouseY-pmouseY)/(mouseX-pmouseX))
+    // strokeWeight(abs(mouseY-pmouseY)/(mouseX-pmouseX))
     line(pmouseX, pmouseY, mouseX, mouseY);
   }
 }
